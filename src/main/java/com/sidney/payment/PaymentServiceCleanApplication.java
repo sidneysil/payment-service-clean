844bc6a1e03935402f885902bf0018f98cc571aa
Sidney da Silva <sidneysil01@gmal.com>
2026-09-22 07:12:42 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/sidney/payment/PaymentServiceCleanApplication.java
@@ -0,0 +1,13 @@
+package com.sidney.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidney/payment/PaymentServiceCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidney.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
